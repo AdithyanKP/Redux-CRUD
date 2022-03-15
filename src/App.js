e44393855775a3c506806d318d25a678eb6fdd2f
Adithyan k P <adithyankp777@gmail.com>
2022-03-15 17:22:48 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { addUser } from "./redux/Users";
 import "./App.css";
 const App = () => {
+  
+  const [name,setName]=useState('')
+  const[username,setUserName] =useState('');
+
   //user Data//
   const userList = useSelector((state) => state.users.value);
   console.log(userList);
+
   //dispatch
   const dispatch = useDispatch();
 
+  //for adding user//
+  const addUsers=()=>{
+    dispatch(addUser({id:userList[userList.length - 1].id + 1,name,username}))
+    setName('')
+    setUserName('')
+  }
+  
   return (
     <>
       <div className="App">
         <div className="addUser">
-          <input type="text" placeholder="...Name" />
-          <input type="text" placeholder="...Username" />
-          <button>Add User</button>
+          <input value={name} type="text" placeholder="...Name" onChange={(e)=>setName(e.target.value)} />
+          <input value={username} type="text" placeholder="...Username" onChange={(e)=>setUserName(e.target.value)}/>
+          <button onClick={()=>addUsers()}>Add User</button>
         </div>
         <div className="displayUsers">
           {userList.map((user) => {
-            console.log("user:", user.name);
-            return <h1>{user.name}</h1>;
+            console.log("user:", user.username);
+            return( <div> <h1>{user.name}</h1>
+            <h1>{user.username}</h1></div>) 
           })}
         </div>
       </div>
--- src/redux/Users.js
+++ src/redux/Users.js
@@ -4,8 +4,12 @@ import { UsersData } from "../fakeData";
 export const userSlice = createSlice({
   name: "users",
   initialState: { value: UsersData },
-
-  addUser: (state, action) => {},
+  reducers: {
+    addUser: (state, action) => {
+      state.value.push(action.payload);
+    },
+  },
 });
 
+export const { addUser } = userSlice.actions;
 export default userSlice.reducer;
