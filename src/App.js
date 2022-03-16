cd096c50c31e6b99f6b81af4d86d875d42fed7a3
Adithyan k P <adithyankp777@gmail.com>
2022-03-16 12:07:09 +0530

    delete,update added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React,{useState} from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { addUser } from "./redux/Users";
+import { addUser,deleteUser, updateUser } from "./redux/Users";
 import "./App.css";
 const App = () => {
   
   const [name,setName]=useState('')
   const[username,setUserName] =useState('');
+const[newUserName,setNewUserName]=useState('')
 
-  //user Data//
+  //user Data
   const userList = useSelector((state) => state.users.value);
   console.log(userList);
 
   //dispatch
   const dispatch = useDispatch();
 
-  //for adding user//
+  //for adding user
   const addUsers=()=>{
-    dispatch(addUser({id:userList[userList.length - 1].id + 1,name,username}))
+    dispatch(addUser({id:userList.length + 1,name,username}))
     setName('')
     setUserName('')
   }
+
+//deleting user
+const deleteUsers =(userId)=>{
+  dispatch(deleteUser({id:userId}))
+}
+
+//updating users
+const updateUsers =(newUserName,Id)=>{
+  dispatch(updateUser({userName:newUserName,id:Id}))
+  
+}
   
   return (
     <>
@@ -29,11 +41,16 @@ const App = () => {
           <input value={username} type="text" placeholder="...Username" onChange={(e)=>setUserName(e.target.value)}/>
           <button onClick={()=>addUsers()}>Add User</button>
         </div>
+      
         <div className="displayUsers">
           {userList.map((user) => {
             console.log("user:", user.username);
             return( <div> <h1>{user.name}</h1>
-            <h1>{user.username}</h1></div>) 
+            <h1>{user.username}</h1>
+            <input   type="text" placeholder="New UserName" onChange={(e)=>(setNewUserName(e.target.value))} />
+            <button onClick={()=>updateUsers(newUserName,user.id)}>update Username</button> <button onClick={()=>deleteUsers(user.id)}>Delete user</button></div>
+            
+            ) 
           })}
         </div>
       </div>
